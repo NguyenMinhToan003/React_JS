5529228ea82a0d1e962a05b71032857451e770c4
Toan <nguyentoan04.0003@gmail.com>
2023-07-13 17:23:59 +0700

    #10.5

--- src/views/examples/MyComponent.js
+++ src/views/examples/MyComponent.js
@@ -1,6 +1,24 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    email: "",
+    password: "",
+  };
+  handerEmail = (event) => {
+    this.setState({
+      email: event.target.value,
+    });
+  };
+  handerPassword = (event) => {
+    this.setState({
+      password: event.target.value,
+    });
+  };
+  handerSubmit = (event) => {
+    event.preventDefault();
+    alert(`email: ${this.state.email}, password: ${this.state.password}`);
+  };
   render() {
     return (
       <form style={{ border: "1px solid #ccc" }}>
@@ -8,37 +26,41 @@ class MyComponent extends React.Component {
           <h1>Sign Up</h1>
           <p>Please fill in this form to create an account.</p>
           <hr />
-
           <label htmlFor="email">
             <b>Email</b>
           </label>
-          <input type="text" placeholder="Enter Email" name="email" required />
-
+          <input
+            type="text"
+            placeholder="Enter Email"
+            value={this.state.email}
+            onChange={(event) => this.handerEmail(event)}
+          />
           <label htmlFor="psw">
             <b>Password</b>
           </label>
           <input
             type="password"
             placeholder="Enter Password"
-            name="password"
-            required
+            onChange={(event) => this.handerPassword(event)}
           />
-
           <label htmlFor="psw-repeat">
             <b>Repeat Password</b>
           </label>
+          ``
           <input
             type="password"
             placeholder="Repeat Password"
-            name="repeatPassword"
-            required
+            value={this.state.password}
+            onChange={(event) => this.handerPassword(event)}
           />
-
           <div className="clearfix">
             <button type="button" className="cancelbtn">
               Cancel
             </button>
-            <button type="submit" className="signupbtn">
+            <button
+              type="submit"
+              className="signupbtn"
+              onClick={(event) => this.handerSubmit(event)}>
               Sign Up
             </button>
           </div>
